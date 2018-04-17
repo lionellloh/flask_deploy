222b40b6f8701cfa6aa9d088733af4c71d038b28
New <new@Lohs-MacBook-Air.local>
2018-04-18 01:20:11 +0800

    app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def main(): 
-	return "heroku deployed haha jiabai zzz!"
+# @app.route("/")
+# def main():
+# 	return "heroku deployed haha jiabai zzz!"
 
 @app.route('/')
 
