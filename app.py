076174583c1553f80f36f67d5ebcb92e0e0bd9a4
New <new@Lohs-MacBook-Air.local>
2018-04-18 01:10:30 +0800

    edited app

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-
 def main(): 
-	return "heroku deployed haha zzz!"
+	return "heroku deployed haha jiabai zzz!"
 
 if __name__ == "__main__": 
 	app.run()
