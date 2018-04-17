e41f59c347f966b48fa45a9d2ea25285811b4bd2
New <new@Lohs-MacBook-Air.local>
2018-04-18 02:31:34 +0800

    import os

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-
+import os
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['postgresql://user:secret@localhost/beentheredumpedthat']
 db = SQLAlchemy(app)
 
 
