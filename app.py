f3310ddae9165fd7de4dd41925fe85380167cfcf
New <new@Lohs-MacBook-Air.local>
2018-04-18 03:23:27 +0800

    database created

--- app.py
+++ app.py
@@ -5,7 +5,17 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    email = db.Column(db.String(120), unique=True)
 
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
 
 # from flask import Flask, render_template, request
 # from flask.ext.sqlalchemy import SQLAlchemy
